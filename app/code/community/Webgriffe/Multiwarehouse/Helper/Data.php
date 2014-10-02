629d8234d1a9d682a8474501e9d9d0f28170e61e
Alessandro Ronchi <aleron75@gmail.com>
2014-10-02 16:30:42 +0200

    Create module skeleton

---
+++ app/code/community/Webgriffe/Multiwarehouse/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alessandro
+ * Date: 10/2/14
+ * Time: 4:25 PM
+ */ 
+class Webgriffe_Multiwarehouse_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
